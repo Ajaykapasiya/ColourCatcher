ff281ee5aa9b99bbfd8f788b3483088116e16c9d
ajay <ajaykumar290904@gmail.com>
2026-02-04 17:43:51 +0530

    add game functionality and styling with initial JavaScript setup

---
+++ myscript.js
@@ -0,0 +1,13 @@
+let targetColor = "";
+let score = 0;
+let time = 30;
+let timer;
+let color = ['red','orange','blue','green','black','grey','hotpink','purple','white','yellow','lightgreen','khaki','maroon','browm','navy','indigo'];
+
+const grid = document.getElementById("grid");
+const targetColorDisplay = document.getElementById("targetColour")
+
+function startGame(){
+    score:0;
+    time:30;
+}
